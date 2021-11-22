d7ae53d4c58a473093621ace6fafcda0d4088943
Natalie Leong <natalieleong19@gmail.com>
2021-11-22 21:54:59 +0800

    static.json

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 app.use(compression());
 app.use(express.static(path.join(__dirname, 'dist')));
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.js'));
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 const PORT = process.env.PORT || 8080;
