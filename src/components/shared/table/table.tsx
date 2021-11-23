1e9b8c34bb84b207959b13a4341336f7621d98a3
Natalie Leong <natalieleong19@gmail.com>
2021-11-24 00:32:05 +0800

    added flex display to tr in displaywaiting for patient database

--- src/components/shared/table/table.tsx
+++ src/components/shared/table/table.tsx
@@ -65,7 +65,7 @@ const Table: React.FC<TableProps> = ({columns, width, filteredData, visibility='
                             
                         )
                         
-                    }): <tr>{DisplayWaiting()}</tr>
+                    }): <tr style={{ display: 'flex', alignItems: 'center'}}>{DisplayWaiting()}</tr>
                  }
                  
             </tbody>       
