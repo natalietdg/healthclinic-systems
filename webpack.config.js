f8aa087552e49780d65e553ac3c30cad88cb6006
Natalie Leong <natalieleong19@gmail.com>
2021-11-25 00:36:51 +0800

    changes to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,7 @@ module.exports = {
         publicPath: '/',
     },
     plugins: [
+        new CompressionPlugin(),
         new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
         new HtmlWebpackPlugin({
           title: 'Output Management',
@@ -61,7 +62,30 @@ module.exports = {
         minimizer: [
             new HtmlMinimizerWebpackPlugin(),
             new TerserPlugin({
-                parallel: true
+                parallel: true,
+                terserOptions: {
+                    parse: {
+                        ecma: 8
+                    },
+                    compress: {
+                        ecma: 5,
+                        warnings: false,
+                        comparisons: false,
+                        inline: 2
+                    },
+                    mangle: {
+                        safari10: true
+                    },
+                    output: {
+                        ecma: 5,
+                        comments: false,
+                        ascii_only: true
+                    },
+                },
+                cache: true,
+                parallel: true,
+                sourceMap: true,
+                extractComments: false,
             }),
             new JsonMinimizerPlugin()
         ]
